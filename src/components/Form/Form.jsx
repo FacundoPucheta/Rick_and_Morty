7ec2752a5a6560f0448fee488676bea2f49e6ef9
Facundo Pucheta <facundopucheta94@gmail.com>
2023-04-05 20:23:52 -0300

    Control added to form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 const Form = () => {
 
+    const [userData, setUserData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        })
+    }
 
     return (
         <form>
             {/* aca puede ir alguna imagen cuando hagas estilos para que quede piola */}
         <label htmlFor="email">EMAIL: </label>
-        <input type="email" />
+        <input name="email" type="email" placeholder="Ingrese su email" value={userData.email} onChange={handleChange} />
         <br></br>
         <label htmlFor="password">PASSWORD: </label>
-        <input type="text" />
+        <input name="password" type="text" placeholder="Ingrese su contraseña" value={userData.password} onChange={handleChange} />
 
         <button>Submit</button>
         </form>
